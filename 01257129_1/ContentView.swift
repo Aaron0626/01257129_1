c375cdf31a021dcd8f0a4dd6086441a0fd5424d1
Aaron0626 <dora9402@gmail.com>
2025-12-30 20:22:54 +0800

    Initial Commit

---
+++ 01257129_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01257129_1
+//
+//  Created by user10 on 2025/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01257129_1/_1257129_1App.swift
@@ -0,0 +1,17 @@
+//
+//  _1257129_1App.swift
+//  01257129_1
+//
+//  Created by user10 on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct _1257129_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
